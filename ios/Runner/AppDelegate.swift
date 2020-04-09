b609d4234f1deb570d5f3dac4d4142916f281727
Fralez <franco.gonzalez.burgos@gmail.com>
2020-04-09 12:40:58 -0300

    LocalNotificationService created to modularise the service - AppDelegate.swift integrates local_notifications - AndroidManifest.xml integrates local_notifications - LocalNotificationService.dart service added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,9 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:bien_aca_quarantine/components/pages/inner_page.dart';
 import 'package:bien_aca_quarantine/components/pages/home_page.dart';
@@ -9,7 +9,7 @@ import 'package:bien_aca_quarantine/components/pages/alert_page.dart';
 import 'package:bien_aca_quarantine/constants/BienAcaConstants.dart';
 import 'package:bien_aca_quarantine/services/UserService.dart';
 import 'package:bien_aca_quarantine/services/GeofencingService.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:bien_aca_quarantine/services/LocalNotificationService.dart';
 
 void main() {
   runApp(BienAcaConstants(
@@ -25,7 +25,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
   String initialRoute = '/';
   bool goToInnerPage = false;
 
@@ -42,7 +41,28 @@ class _MyAppState extends State<MyApp> {
         await startGeofencing(10.0);
       }
     });
-    setupNotificationPlugin();
+    initializeLocalNotifications(onDidReceiveLocalNotification);
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            onPressed: () async {
+              print("On pressed");
+            },
+            child: Text('Ok'),
+          )
+        ],
+      ),
+    );
   }
 
   @override
@@ -67,90 +87,6 @@ class _MyAppState extends State<MyApp> {
                 fontSize: 16.0, color: BienAcaConstants.of(context).blue),
           )),
       home: goToInnerPage ? InnerPage() : HomePage(),
-    );
-  }
-
-//  todo: podríamos sacar el async await
-  void setupNotificationPlugin() async {
-    FlutterLocalNotificationsPlugin localNotificationInstance =
-        FlutterLocalNotificationsPlugin();
-
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('icon');
-
-    var initializationSettingsIOS = IOSInitializationSettings(
-      requestSoundPermission: true,
-      requestBadgePermission: true,
-      requestAlertPermission: true,
-      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
-    );
-
-    var initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
-
-    await localNotificationInstance.initialize(initializationSettings,
-        onSelectNotification: onSelectNotification);
-
-    setState(() {
-      flutterLocalNotificationsPlugin = localNotificationInstance;
-    });
-    print('< ==== i am inside intialization ===>');
-    print(localNotificationInstance);
-    setupNotification();
-  }
-
-  void setupNotification() async {
-    print(flutterLocalNotificationsPlugin);
-    var time = Time(10, 14, 0);
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'repeatDailyAtTime channel id',
-        'repeatDailyAtTime channel name',
-        'repeatDailyAtTime description');
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.showDailyAtTime(
-        0,
-        'show daily title',
-        'Daily notification shown at approximately',
-        time,
-        platformChannelSpecifics);
-  }
-
-  Future onSelectNotification(String payload) async {
-    if (payload != null) {
-      debugPrint('notification payload: ' + payload);
-    }
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SuccessPage()),
-    );
-  }
-
-  Future onDidReceiveLocalNotification(
-      int id, String title, String body, String payload) async {
-    // display a dialog with the notification details, tap ok to go to another page
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text(title),
-        content: Text(body),
-        actions: [
-          CupertinoDialogAction(
-            isDefaultAction: true,
-            child: Text('Ok'),
-            onPressed: () async {
-              Navigator.of(context, rootNavigator: true).pop();
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SuccessPage(),
-                ),
-              );
-            },
-          )
-        ],
-      ),
     );
   }
 }
