5f3a88ec17894a061b92b1d2b5671a0a0a274294
mau <maurodibert@gmail.com>
2020-04-10 14:49:13 -0300

    add biometrics wip1

---
+++ lib/components/pages/error_page_not_authenticated.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:bien_aca_quarantine/components/layouts/design_layout.dart';
+
+import 'package:bien_aca_quarantine/constants/BienAcaConstants.dart';
+
+class ErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double cWidth = MediaQuery
+        .of(context)
+        .size
+        .width * 0.7;
+    return Scaffold(
+        backgroundColor: BienAcaConstants.of(context).lightPink,
+        body: DesignLayout(
+            content: Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.report_problem,
+                    color: Colors.white,
+                    size: 40.0,
+                  ),
+                  SizedBox(width: 20.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(BienAcaConstants.of(context).errorPageBodyTitle,
+                          style: TextStyle(
+                              fontSize: 28.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'RobotoSlab',
+                              color: Colors.white)),
+                      Container(
+                        width: cWidth,
+                        child: Text(BienAcaConstants.of(context).errorPagePageBody,
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.white)),
+                      ),
+                      SizedBox(height: 20.0),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+            ],
+          ),
+        )));
+  }
+}
---
+++ lib/services/models/AuthenticationProof.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:bien_aca_quarantine/services/models/User.dart';
+
+final serverUrl = 'https://bian-aca-prod.herokuapp.com';
+
+Future<Heartbeat> sendHeartbeat(lat, lng) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  User user = User.fromJson(jsonDecode(prefs.getString("user")));
+  if (user == null) return Future.value(null);
+
+  Heartbeat heartbeat = Heartbeat(
+      userId: user.id,
+      time: DateTime.now().toIso8601String(),
+      lat: lat,
+      lng: lng);
+
+  final jsonHeartbeat = jsonEncode(heartbeat);
+
+  final response = await http.post('$serverUrl/users/${user.id}/heartbeats',
+      headers: {"Content-Type": "application/json"}, body: jsonHeartbeat);
+
+  if (response.statusCode == 201) {
+    Heartbeat heartbeat =
+        Heartbeat.fromJson(json.decode(response.body)["heartbeat"]);
+    return Future.value(heartbeat);
+  } else {
+    throw Exception('Failed to send the heartbeat');
+  }
+}
+
+class Heartbeat {
+  final int id;
+  final int userId;
+  final String time;
+  final double lat;
+  final double lng;
+  final bool withinFence;
+
+  Heartbeat(
+      {this.id, this.userId, this.time, this.lat, this.lng, this.withinFence});
+
+  factory Heartbeat.fromJson(Map<String, dynamic> json) {
+    return Heartbeat(
+      id: json['id'],
+      userId: json['user_id'],
+      time: json['time'],
+      lat: double.parse(json['lat']),
+      lng: double.parse(json['lng']),
+      withinFence: json['within_fence'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'heartbeat': {
+        'id': id,
+        'time': time,
+        'lat': lat,
+        'lng': lng,
+      }
+    };
+  }
+}
