1d37e71e80fd3c0b1a8e23386c591dc2b726d2f9
Fralez <fralez@pop-os.localdomain>
2020-04-16 14:20:58 -0300

    Send Heartbeat every half an hour

--- lib/services/models/Heartbeat.dart
+++ lib/services/models/Heartbeat.dart
@@ -7,17 +7,26 @@ import 'package:bien_aca_quarantine/services/models/User.dart';
 
 final serverUrl = 'https://bian-aca-prod.herokuapp.com';
 
-Future<Heartbeat> sendHeartbeat(lat, lng) async {
+Future<Heartbeat> sendHeartbeat(lat, lng, {bool isAliveHeartbeat}) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
   User user = User.fromJson(jsonDecode(prefs.getString("user")));
   if (user == null) return Future.value(null);
 
+  Map<String, dynamic> data;
+
+  if(isAliveHeartbeat) {
+    data = {
+      'isAliveHeartbeat': true
+    };
+  }
+ 
   Heartbeat heartbeat = Heartbeat(
       userId: user.id,
       time: DateTime.now().toIso8601String(),
       lat: lat,
-      lng: lng);
+      lng: lng,
+      data: data);
 
   final jsonHeartbeat = jsonEncode(heartbeat);
 
@@ -40,9 +49,10 @@ class Heartbeat {
   final double lat;
   final double lng;
   final bool withinFence;
+  final Map<String, dynamic> data;
 
   Heartbeat(
-      {this.id, this.userId, this.time, this.lat, this.lng, this.withinFence});
+      {this.id, this.userId, this.time, this.lat, this.lng, this.withinFence, this.data});
 
   factory Heartbeat.fromJson(Map<String, dynamic> json) {
     return Heartbeat(
@@ -52,6 +62,7 @@ class Heartbeat {
       lat: double.parse(json['lat']),
       lng: double.parse(json['lng']),
       withinFence: json['within_fence'],
+      data: json['data']
     );
   }
 
@@ -62,6 +73,7 @@ class Heartbeat {
         'time': time,
         'lat': lat,
         'lng': lng,
+        'data': data
       }
     };
   }
